8ca0bb47aec662791964a9028b4f99d13c6dec19
Mark Bain <mark.bain@perkmylife.com>
2024-07-21 11:27:08 +0200

    Refactored MultiOnUtils

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from dotenv import load_dotenv
 from src import multion
+
+load_dotenv()
+
 def main():
-    session_id = multion.visit_event_website()
-    multion.scrap_website(session_id)
+    multionscrapper = multion.MultiOnUtils()
+    session_id = multionscrapper.visit_event_website()
+    multionscrapper.scrap_website(session_id)
 
 if __name__ == "__main__":
     main()
--- src/multion.py
+++ src/multion.py
@@ -2,42 +2,35 @@
 # Make sure that the MultiOn Chrome Extension is installed and enabled (for more details, see here).
 
 import os
-from dotenv import load_dotenv
 from multion.client import MultiOn
 
+class MultiOnUtils:
+    def __init__(self):
+        self.multion_api_key = os.environ.get("MULTION_API_KEY") # Get your API key from https://app.multion.ai/api-keys
+        if not self.multion_api_key:
+            raise ValueError("MULTION_API_KEY is not set in .env variables")
+    def visit_event_website(self):
+        client = MultiOn(api_key=self.multion_api_key)
+        # client = MultiOn(api_key=multion_api_key,agentops_api_key=os.environ.get("AGENTOPS_API_KEY"))
 
-load_dotenv()
+        create_response = client.sessions.create(
+            url=os.environ.get("EVENT_WEBSITE"),
+            local=True
+        )
+        session_id = create_response.session_id
 
-def visit_event_website():
-    multion_api_key = os.environ.get("MULTION_API_KEY") # Get your API key from https://app.multion.ai/api-keys
-    if not multion_api_key:
-        raise ValueError("MULTION_API_KEY is not set in .env variables")
+        return session_id
 
-    client = MultiOn(api_key=multion_api_key)
-    # client = MultiOn(api_key=multion_api_key,agentops_api_key=os.environ.get("AGENTOPS_API_KEY"))
+    def scrap_website(self, session_id):
+        client = MultiOn(api_key=self.multion_api_key)
+        # client = MultiOn(api_key=multion_api_key,agentops_api_key=os.environ.get("AGENTOPS_API_KEY"))
 
-    create_response = client.sessions.create(
-        url=os.environ.get("EVENT_WEBSITE"),
-        local=True
-    )
-    session_id = create_response.session_id
+        retrieve_response = client.retrieve(
+            url=os.environ.get("EVENT_WEBSITE"),
+            cmd="For the event, get its: name, date, venue, judges, sponsors, prizes",
+            fields=["website_url", "name", "date", "venue", "judges", "sponsors", "prizes"]
+        )
 
-    return session_id
-
-def scrap_website(session_id):
-    multion_api_key = os.environ.get("MULTION_API_KEY") # Get your API key from https://app.multion.ai/api-keys
-    if not multion_api_key:
-        raise ValueError("MULTION_API_KEY is not set in .env variables")
-
-    client = MultiOn(api_key=multion_api_key)
-    # client = MultiOn(api_key=multion_api_key,agentops_api_key=os.environ.get("AGENTOPS_API_KEY"))
-
-    retrieve_response = client.retrieve(
-        url=os.environ.get("EVENT_WEBSITE"),
-        cmd="For the event, get its: name, date, venue, judges, sponsors, prizes",
-        fields=["website_url", "name", "date", "venue", "judges", "sponsors", "prizes"]
-    )
-
-    data = retrieve_response.data
-    print(data)
-    return data
+        data = retrieve_response.data
+        print(data)
+        return data
