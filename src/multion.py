e2c97aa526f3a3c771027f51dbd173c8cfa63bee
Mark Bain <mark.bain@perkmylife.com>
2024-07-21 12:02:26 +0200

    Added agentops

--- src/multion.py
+++ src/multion.py
@@ -7,11 +7,11 @@ from multion.client import MultiOn
 class MultiOnUtils:
     def __init__(self):
         self.multion_api_key = os.environ.get("MULTION_API_KEY") # Get your API key from https://app.multion.ai/api-keys
+        self.agentops_api_key = os.environ.get("AGENTOPS_API_KEY")  # Get your API key from https://app.agentops.ai/settings/projects
         if not self.multion_api_key:
             raise ValueError("MULTION_API_KEY is not set in .env variables")
     def visit_event_website(self):
-        client = MultiOn(api_key=self.multion_api_key)
-        # client = MultiOn(api_key=multion_api_key,agentops_api_key=os.environ.get("AGENTOPS_API_KEY"))
+        client = MultiOn(api_key=self.multion_api_key, agentops_api_key=self.agentops_api_key)
 
         create_response = client.sessions.create(
             url=os.environ.get("EVENT_WEBSITE"),
@@ -22,8 +22,7 @@ class MultiOnUtils:
         return session_id
 
     def scrap_website(self, session_id):
-        client = MultiOn(api_key=self.multion_api_key)
-        # client = MultiOn(api_key=multion_api_key,agentops_api_key=os.environ.get("AGENTOPS_API_KEY"))
+        client = MultiOn(api_key=self.multion_api_key, agentops_api_key=self.agentops_api_key)
 
         retrieve_response = client.retrieve(
             url=os.environ.get("EVENT_WEBSITE"),
